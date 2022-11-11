1193f87ddc2f4f372349c48efb402ff0e7db5c17
Corbin Brown <cjbrown019@gmail.com>
2022-11-10 18:47:00 -0600

    Finished Character Sheet

--- Dungeon/DungeonLibrary/Character.cs
+++ Dungeon/DungeonLibrary/Character.cs
@@ -2,7 +2,73 @@
 {
     public class Character
     {
-        public int Life { get; set; }
+        private int _life;
+
+        public string Name { get; set; }
+
+        public int HitChance { get; set; }
+
+        public int Block { get; set; }
+
+        public int MaxLife { get; set; }
+
+        public int Life
+        {
+            get { return _life; }
+            set
+            {
+                if (value >= MaxLife)
+                {
+                    value = MaxLife;
+                }
+                else
+                {
+                    value = _life;
+                }
+
+            }
+
+
+        }
+
+        public Character()
+        {
+            
+        }
+
+        public Character(string name, int hitChance, int block, int maxLife, int life)
+        {
+            Name = name;
+            HitChance = hitChance;
+            Block = block;
+            MaxLife = maxLife;
+            Life = life;
+        }
+
+        public int CalcBlock()
+        {
+            return Block;
+        }
+
+        public int CalcHitChance()
+        {
+            return HitChance;
+        }
+
+        public int CalcDamage()
+        {
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}\n" +
+                "Max Life: {1}\n" +
+                "Life: {2}\n" +
+                "Block: {3}\n" +
+                "Hit Chance: {4}");
+        }
 
     }
+
 }
