a4b20081c16f1a98c08fe7eade67cf25dbca8fa4
Corbin Brown <cjbrown019@gmail.com>
2022-11-15 20:35:01 -0600

    Added Monster and Player

---
+++ Dungeon/DungeonLibrary/Monster.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    internal class Monster : Character
+    {
+        private int _minDamage;
+
+        public int MaxDamage { get; set; }
+        public int MinDamage
+        {
+            get { return _minDamage; }
+            set
+            {
+                if (value > MaxDamage)
+                {
+                    _minDamage = MaxDamage;
+                }
+                else
+                {
+                    value = _minDamage;
+                }
+            }
+        }
+
+        public string Description { get; set; }
+
+        public Monster(string name,int hitChance, int block, int maxLife, int life, int maxDamage, int minDamage, string description):base(name, hitChance, block, life, maxLife)  
+        {
+            
+            MaxDamage = maxDamage;
+            MinDamage = minDamage;
+            Description = description;
+        }
+
+
+        public override int CalcDamage()
+        {
+            Random random = new Random();
+
+            int damage = random.Next(MaxDamage, MinDamage + 1);
+            return damage;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\nMax Damage: {MaxDamage} \nMin Damage: {MinDamage}\nDescription: {Description}";
+        }
+
+    }
+}
---
+++ Dungeon/DungeonLibrary/Player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Player : Character
+    {
+        public Races Races { get; set; }
+
+        public Weapon EquipedWeapon { get; set; }
+
+        public Player(string name, int hitChance, int block, int life, int maxLife, Races races, Weapon equipedWeapon):base(name, hitChance, block, life, maxLife)  
+        {
+            Races = races;
+            EquipedWeapon = equipedWeapon;
+        }
+
+
+        public override string ToString()
+        {
+            string raceDescription = "";
+            switch (Races)
+            {
+                case Races.Human:
+                    raceDescription = "A normal dude";
+                    break;
+                case Races.Orc:
+                    raceDescription = "AND MY AXE";
+                    break;
+                case Races.Elf:
+                    raceDescription = "A regal, magical being";
+                    break;
+                default:
+                    break;
+
+                    
+            }
+
+            return base.ToString() + "\nDesctiption: " + raceDescription + "\nWeapon: " + EquipedWeapon;
+        }
+
+
+        public override int CalcDamage()
+        {
+            Random random = new Random();
+
+            int damage = random.Next(EquipedWeapon.MinDamage,EquipedWeapon.MaxDamage + 1);
+            return damage;
+        }
+
+        public override int CalcHitChance()
+        {
+            return base.CalcHitChance() + EquipedWeapon.BonusHitChance;
+        }
+
+    }
+}
