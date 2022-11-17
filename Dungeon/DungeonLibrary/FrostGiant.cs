6d7e553a841d8f747a3d6efc8fa899540a7b8730
Corbin Brown <cjbrown019@gmail.com>
2022-11-16 18:19:49 -0600

    finished and fixed  Monster

---
+++ Dungeon/DungeonLibrary/FrostGiant.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    internal class FrostGiant : Monster
+    {
+        public bool Old { get; set; }
+        public bool ThickSkin { get; set; }
+
+        public FrostGiant(string name, int hitChance, int block, int maxLife, int life, int maxDamage, int minDamage, string description, bool old, bool thickSkin) : base(name, hitChance, block, maxLife, life, maxDamage, minDamage, description)
+        {
+            Old = old;
+            ThickSkin = thickSkin;
+        }
+
+        public FrostGiant()
+        {
+            Name = "Frost Giant";
+            HitChance = 10;
+            Block = 10;
+            MaxLife = 100;
+            Life = 100;
+            MaxDamage = 50;
+            MinDamage = 1;
+            Description = "Giant blue ";
+            Old = false;
+            ThickSkin = false;
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\nOld: {Old}\nThick Skin: {ThickSkin}";
+        }
+
+        public override int CalcBlock()
+        {
+
+            if (ThickSkin)
+            {
+                return Block += Block;
+            }
+            else
+            {
+                return Block;
+            }
+
+        }
+
+        public override int CalcDamage()
+        {
+            Random random = new Random();
+
+            int damage = random.Next(MinDamage, MaxDamage + 1);
+            if (Old)
+            {
+                return damage / 10;
+            }
+            else
+            {
+                return damage;
+            }
+        }
+    }
+}
---
+++ Dungeon/DungeonLibrary/Orcs.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Orcs : Monster
+    {
+        public bool Strong { get; set; }
+        public bool ThickSkin { get; set; }
+
+        public Orcs( string name, int hitChance, int block, int maxLife, int life, int maxDamage, int minDamage, string description, bool strong, bool thickSkin) :base(name,hitChance,block,maxLife,life,maxDamage,minDamage,description)
+        {
+            Strong = strong;
+            ThickSkin = thickSkin;
+        }
+
+        public Orcs()
+        {
+            Name = "Orc Soldier";
+            HitChance = 30;
+            Block = 10;
+            MaxLife = 100;
+            Life = 100;
+            MaxDamage = 20;
+            MinDamage = 1;
+            Description = "A big hairy grey huminoid Orc with tattered clothes.";
+            Strong = false;
+            ThickSkin = false;
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\nStrong: {Strong}\nThick Skin: {ThickSkin}";
+        }
+
+        public override int CalcBlock()
+        {
+
+            if (ThickSkin)
+            {
+                return Block += Block;
+            }
+            else
+            {
+                return Block;
+            }
+            
+        }
+
+        public override int CalcDamage()
+        {
+            Random random = new Random();
+
+            int damage = random.Next(MinDamage, MaxDamage + 1);
+            if (Strong)
+            {
+                return damage + 10;
+            }
+            else
+            {
+                return damage;
+            }
+        }
+    }
+
+
+}
